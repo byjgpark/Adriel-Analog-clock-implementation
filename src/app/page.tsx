b754ea72ba9728afaeaadc7d8222745da56b0e05
Jungi Park <byjungy93@gmail.com>
2024-01-21 13:12:59 +0900

    udpate timeSlice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,36 +10,33 @@ import SecHandStyles from "@/styles/components/SecHandStyles";
 import { useAppSelector, useAppDispatch, useAppStore } from "@/lib/hooks";
 import { Provider } from "react-redux";
 import { makeStore } from "@/lib/store";
-import { increment } from "@/lib/features/time/timeSlice";
+import { updateDate} from "@/lib/features/time/timeSlice";
 
 export default function Home() {
   const [date, setDate] = useState(new Date());
 
-  const count = useAppSelector((state) => state.time.value);
+  //const date = useAppSelector((state) => state.time.date);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
 
     const timerId = setInterval(() => {
       setDate(new Date());
+      dispatch(updateDate());
     }, 1000);
 
-    dispatch(increment());
+    //dispatch(increment());
 
-    console.log("check date", date)
-
-    console.log("check date", typeof date)
-    
-
+    //console.log("check date Hr type", typeof date.getHours());
 
     // Get time
-    const time = [date.getHours(), date.getMinutes(), date.getSeconds()];
-    const [h, m, s] = time;
+    // const time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    // const [h, m, s] = time;
 
-    // Get angles
-    const degHour = h * (360 / 12) + m * (360 / 12 / 60);
-    const degMin = m * (360 / 60) + s * (360 / 60 / 60);
-    const degSec = s * (360 / 60);
+    // // Get angles
+    // const degHour = h * (360 / 12) + m * (360 / 12 / 60);
+    // const degMin = m * (360 / 60) + s * (360 / 60 / 60);
+    // const degSec = s * (360 / 60);
 
     return () => clearInterval(timerId);
   }, [date]);
@@ -49,7 +46,7 @@ export default function Home() {
       {/* <HrHandStyles degree={degHour}></HrHandStyles>
       <MinHandStyles degree={degMin}></MinHandStyles>
       <SecHandStyles degree={degSec}></SecHandStyles> */}
-      <div>{count}</div>
+      {/* <div>{date.getDate()}</div> */}
     </ClockStyles>
   );
 }
--- src/lib/features/time/timeSlice.ts
+++ src/lib/features/time/timeSlice.ts
@@ -3,16 +3,26 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '@/lib/store';
 import { ObjectHTMLAttributes } from 'react';
 
+interface timeState {
+    currentHr: number;
+    currentMin: number;
+    currentSec: number;
+}
+
 interface CounterState {
   // value: Object;
-  value: number;
-  time: Object;
+  time: timeState;
 }
+
+
 
 const initialState: CounterState = {
   // value: new Date,
-  value: 0,
-  time: {},
+  time:  {
+    currentHr: 0,
+    currentMin: 0,
+    currentSec: 0,
+  }
 }
 
 export const timeSlice = createSlice({
@@ -20,24 +30,31 @@ export const timeSlice = createSlice({
     // `createSlice` will infer the state type from the `initialState` argument
     initialState,
     reducers: {
-      increment: (state) => {
-        state.value += 1
-      },
-      decrement: (state) => {
-        state.value -= 1
-      },
-      // Use the PayloadAction type to declare the contents of `action.payload`
-      incrementByAmount: (state, action: PayloadAction<number>) => {
-        state.value += action.payload
-      },
 
-      
+    //   updateDate: (state) => {
+    //     state.date = new Date();
+    //   },
+
+    //   decrement: (state) => {
+    //     state.value -= 1
+    //   },
+
+    //   calculateTime: (state, action: PayloadAction<timeState>) => {
+    //     state.value += action.payload
+    //   },
+
+    //   // Use the PayloadAction type to declare the contents of `action.payload`
+    //   incrementByAmount: (state, action: PayloadAction<number>) => {
+    //     state.value += action.payload
+    //   },
+
+
     },
   })
   
-  export const { increment, decrement, incrementByAmount } = timeSlice.actions
+  export const { updateDate } = timeSlice.actions
   
   // Other code such as selectors can use the imported `RootState` type
-  export const selectCount = (state: RootState) => state.time.value
+  export const selectCount = (state: RootState) => state.time.date
   
   export default timeSlice.reducer
